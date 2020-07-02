283a9919bb56b7b7b1308a550b8209deb7d552d4
farhat97 <44099258+farhat97@users.noreply.github.com>
2020-07-02 14:25:55 -0500

    Added filter bar

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { TasksComponent } from './tasks.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
     declarations: [ TasksComponent, TaskListComponent ],
-    imports: [ MatTableModule ],
+    imports: [ MatTableModule, MatSelectModule, MatCheckboxModule ],
     exports: [ TasksComponent, TaskListComponent ],
     providers: [ ]
 })
